67bd9fad35edf3062beba90bfdf4bc1d30fdee8e
Qossay.Rida <aqusay06@gmail.com>
2024-04-22 09:46:36 +0200

    commit 2

---
+++ src/main/java/com/example/tryforjenkins/TryForJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tryforjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryForJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryForJenkinsApplication.class, args);
+    }
+
+}
